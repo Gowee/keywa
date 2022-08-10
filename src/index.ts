d912de5f40d146f3633054bc7c23a0a6f04d1a72
Hung-I Wang <whygowe@gmail.com>
2022-08-10 13:55:27 +0800

    fix: ClientError not found

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,26 @@ const TIMEOUT_IN_SECONDS = 15 * 60;
 const POLL_INTERNAL_IN_SECONDS = 5;
 const DEFAULT_SESSION_NAME = "default";
 
+declare global {
+  const TELEGRAM_CHAT_ID: string;
+  const TELEGRAM_BOT_TOKEN: string;
+}
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  KEYWA: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+
+  TELEGRAM_BOT_TOKEN: string;
+  TELEGRAM_CHAT_ID: string | number;
+}
+
+class ClientError extends Error {}
+
 export default {
   async fetch(
     request: Request,
